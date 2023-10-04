9b5473d88d5668111a1fee450e09996051f52cdd
Antonio Doncel Campos <adoncel@users.noreply.github.com>
2023-10-04 17:20:29 +0200

    finish-footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,35 @@
 import "./Footer.css";
 
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaCopyright } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__left">
+        <a href="https://www.adoncca.nl/">
+          <FaCopyright />
+          <span style={{ marginBottom: 3 }}>Adoncca 2023</span>
+        </a>
+      </div>
+      <div className="footer__right">
+        <a
+          href="https://github.com/adoncel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/adoncel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
