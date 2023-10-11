6fa186aa1936c4d5a48afc0c273aacd2ea18684c
Antonio Doncel Campos <adoncel@users.noreply.github.com>
2023-10-11 16:07:06 +0200

    update-foot-head

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { HashRouter } from "react-router-dom";
 
 import logo from "./assets/logo_nobg.png";
@@ -9,8 +10,22 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div id="header">
+      <div id="header" className="header">
         <img src={logo} className="header__logo" width={"120px"} alt="logo" />
+        <a
+          href="https://github.com/adoncel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/adoncel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div id="main">
         <HashRouter>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,14 @@
 import "./Footer.css";
 
-import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__left">
-        <a href="https://www.adoncca.nl/">
-          <FaCopyright />
-          <span style={{ marginBottom: 3 }}>Adoncca 2023</span>
-        </a>
-      </div>
-      <div className="footer__right">
-        <a
-          href="https://github.com/adoncel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsGithub />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/adoncel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsLinkedin />
-        </a>
-      </div>
+      <a href="https://www.adoncca.nl/">
+        <FaCopyright />
+        <span style={{ marginBottom: 3 }}>Adoncca 2023</span>
+      </a>
     </footer>
   );
 };
