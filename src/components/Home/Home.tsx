8c9e97131451015db787fc411678ab32f029d9ed
Antonio Doncel Campos <adoncel@users.noreply.github.com>
2023-10-12 17:41:25 +0200

    fix-home-sect

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,20 +5,16 @@ const Home = () => {
   return (
     <>
       <div className="home">
-        <div />
-        <div className="home__data">
-          <h1>
-            Hello! I am{" "}
-            <span>
-              <u>Antonio Doncel</u>
-            </span>
-          </h1>
-          <h1>I am a Software Engineer Manager</h1>
-          <NavLink to="/contact">
-            <button>Get in touch</button>
-          </NavLink>
-        </div>
-        <div />
+        <h1>
+          Hello! I am{" "}
+          <span>
+            <u className="home__highlight">Antonio Doncel</u>
+          </span>
+        </h1>
+        <h1>I am a Software Engineer Manager</h1>
+        <NavLink to="/contact">
+          <button>Get in touch</button>
+        </NavLink>
       </div>
     </>
   );
